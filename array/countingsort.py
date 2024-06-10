aa9de59c3e90a67a9df36e94ba9d202b416d7c9d
Thalisson <thalissonjon@gmail.com>
2024-06-09 21:43:07 -0300

    att: counting sort

--- array/countingsort.py
+++ array/countingsort.py
@@ -1,4 +1,5 @@
 arr = [1,0,3,1,3,1]
+# arr = [14, 50, 10, 7, 31, 91, 25, 56, 27, 34]
 
 def countingsort(arr):
     # maximum value
@@ -8,11 +9,12 @@ def countingsort(arr):
             max = i
     
     # arr with range 0 to max
-    auxArr = [] 
-    i=0
-    while i<=max: # <= to add element 0 
-        auxArr.append(0)
-        i+=1
+    auxArr = [0] * max 
+    print(auxArr)
+    # i=0
+    # while i<=max: # <= to add element 0 
+    #     auxArr.append(0)
+    #     i+=1
     
     # element count
     for i in arr:
@@ -26,8 +28,19 @@ def countingsort(arr):
         lastNumber = auxArr[i]
         i+=1
     
-    #shift to the right
+    orderedarr = [0] * len(arr)
+    # print(auxArr)
+
+    for i in range(len(arr)-1, -1, -1):
+        value = arr[i]
+        pos = auxArr[value]-1
+        orderedarr[pos] = value
+
+        auxArr[value] -= 1
+        
     
-    print(auxArr)
+    print(orderedarr)
+    
+    
 
 countingsort(arr)
